394b4f31a77a091905a1bef7d35c14d52724fbbd
Sakshi <74068861+sakshi-cse@users.noreply.github.com>
2023-05-24 18:01:46 +0530

    Create build_angular.sh

---
+++ scripts/build_angular.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change to the project directory
+cd /var/www/html
+
+# Set environment variables, if needed
+# export ENV_VAR_NAME=value
+
+# Build the Angular project
+ng build --prod
