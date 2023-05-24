25ffe46c03e16c7dbfe698145cd7790d5f1a6c08
Sakshi <74068861+sakshi-cse@users.noreply.github.com>
2023-05-24 18:02:24 +0530

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to the project directory
+cd /var/www/html
+
+# Install Node.js dependencies using npm or yarn
+# Replace the package manager command with the appropriate one for your project
+
+# If using npm:
+npm install
+
+# If using yarn:
+# yarn install
+
+# Build the Angular project
+ng build --prod
